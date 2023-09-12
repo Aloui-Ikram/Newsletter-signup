9fe7545a71456f7969892dbc2546f1d870a2ab57
Ikram ALOUI <ikramaloui145@gmail.com>
2023-09-12 02:52:47 -1100

    intial commit

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post("/failure", function(res,req){
   res.redirect("/")
 })
 
-app.listen( process.env.PORT || 3000, function(req,res){
+app.listen( 3000, function(req,res){
   console.log("server is running on port 3000");
 });
 //appi key
